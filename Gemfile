e83853a2963b591edb19294defd1fc1e3baeea07
man <xbcilanin@aol.com>
2025-02-18 01:44:37 -0500

    Initial commit17

--- Gemfile
+++ Gemfile
@@ -19,7 +19,6 @@ gem "jbuilder"
 gem "mysql2"
 
 
-
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations',  sessions: 'users/sessions'  }
   root to: 'home#index'
     get 'dashboard', to: 'dashboard#index', as: 'dashboard'
+    get 'signup', to: 'home#signup'
 
     devise_scope :user do
       get "/users/sign_out", to: "users/sessions#destroy"
